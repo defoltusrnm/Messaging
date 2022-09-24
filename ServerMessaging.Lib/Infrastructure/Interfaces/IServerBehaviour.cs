149a9661507a7fc8d7e1500fb870bce6d91f1a3e
srg_ansmv <defoltusrnm@gmail.com>
2022-09-24 14:12:45 +0200

    delete some things

--- ServerMessaging.Lib/Infrastructure/Interfaces/IServerBehaviour.cs
+++ ServerMessaging.Lib/Infrastructure/Interfaces/IServerBehaviour.cs
@@ -2,6 +2,8 @@
 
 public interface IServerBehaviour : IMessagingBehaviour
 {
+    HashSet<SubscriberInfo> Subscribers { get; }
+    
     Task RunAsync(CancellationToken? cancellationToken = null);
 
     Task StopAsync(CancellationToken? cancellationToken = null);
--- ServerMessaging.Playground/Program.cs
+++ ServerMessaging.Playground/Program.cs
@@ -1,9 +1,1 @@
-﻿using ServerMessaging.Lib;
-using ServerMessaging.Lib.Infrastructure.Interfaces;
-using ServerMessaging.PlaygroundServer;
-using System.Net;
-
-using IServerBehaviour server = new LoggingServer(new TcpServerBehaviour(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000)));
-
-server.RunAsync();
-server.StopAsync();
+﻿
