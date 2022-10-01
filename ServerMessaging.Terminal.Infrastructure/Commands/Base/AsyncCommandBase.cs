faeb133ab51f1f54d6fbb11f8faec589201bb055
srg_ansmv <defoltusrnm@gmail.com>
2022-10-01 08:05:30 +0200

    dwadawdada

---
+++ ServerMessaging.Terminal.Infrastructure/Commands/Base/AsyncCommandBase.cs
@@ -0,0 +1,41 @@
+﻿using ServerMessaging.Terminal.Infrastructure.Commands.Interfaces;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ServerMessaging.Terminal.Infrastructure.Commands.Base;
+
+public abstract class AsyncCommandBase : IAsyncCommand
+{
+    public bool IsRunning { get; protected set; }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public abstract bool CanExecute(object? parameter);
+
+    public abstract Task ExecuteAsync(object? parameter);
+
+    public virtual async void Execute(object? parameter)
+    {
+        try
+        {
+            IsRunning = true;
+            await ExecuteAsync(parameter);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            IsRunning = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    protected void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+}
---
+++ ServerMessaging.Terminal.Infrastructure/Commands/Interfaces/IAsyncCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ServerMessaging.Terminal.Infrastructure.Commands.Interfaces;
+
+public interface IAsyncCommand : ICommand
+{
+    bool IsRunning { get; }
+
+    Task ExecuteAsync(object? parameter);
+}
